2477455a1515f0c1c3b9e7f83b26a8b66743f42a
Jorge Jesús Campoy López <jjcampoy@fidesol.org>
2022-04-19 14:16:52 +0200

    feat: añadidos colores de consola

--- functions/auxiliars.js
+++ functions/auxiliars.js
@@ -35,6 +35,140 @@ function createEndpointObj() {
   };
 }
 
+function coLog() {
+  const colors = {
+    Reset: "\x1b[0m",
+    Bright: "\x1b[1m",
+    Dim: "\x1b[2m",
+    Underscore: "\x1b[4m",
+    Blink: "\x1b[5m",
+    Reverse: "\x1b[7m",
+    Hidden: "\x1b[8m",
+
+    FgBlack: "\x1b[30m",
+    FgRed: "\x1b[31m",
+    FgGreen: "\x1b[32m",
+    FgYellow: "\x1b[33m",
+    FgBlue: "\x1b[34m",
+    FgMagenta: "\x1b[35m",
+    FgCyan: "\x1b[36m",
+    FgWhite: "\x1b[37m",
+
+    BgBlack: "\x1b[40m",
+    BgRed: "\x1b[41m",
+    BgGreen: "\x1b[42m",
+    BgYellow: "\x1b[43m",
+    BgBlue: "\x1b[44m",
+    BgMagenta: "\x1b[45m",
+    BgCyan: "\x1b[46m",
+    BgWhite: "\x1b[47m",
+  };
+
+  return {
+    color: {
+      Reset: (msg) => {
+        console.log(colors["Reset"], msg);
+      },
+      Bright: (msg) => {
+        console.log(colors["Bright"], msg);
+        console.log(colors["Reset"]);
+      },
+      Dim: (msg) => {
+        console.log(colors["Dim"], msg);
+        console.log(colors["Reset"]);
+      },
+      Underscore: (msg) => {
+        console.log(colors["Underscore"], msg);
+        console.log(colors["Reset"]);
+      },
+      Blink: (msg) => {
+        console.log(colors["Blink"], msg);
+        console.log(colors["Reset"]);
+      },
+      Reverse: (msg) => {
+        console.log(colors["Reverse"], msg);
+        console.log(colors["Reset"]);
+      },
+      Hidden: (msg) => {
+        console.log(colors["Hidden"], msg);
+        console.log(colors["Reset"]);
+      },
+      FgBlack: (msg) => {
+        console.log(colors["FgBlack"], msg);
+        console.log(colors["Reset"]);
+      },
+      FgRed: (msg) => {
+        console.log(colors["FgRed"], msg);
+        console.log(colors["Reset"]);
+      },
+      FgGreen: (msg) => {
+        console.log(colors["FgGreen"], msg);
+        console.log(colors["Reset"]);
+      },
+      FgYellow: (msg) => {
+        console.log(colors["FgYellow"], msg);
+        console.log(colors["Reset"]);
+      },
+      FgBlue: (msg) => {
+        console.log(colors["FgBlue"], msg);
+        console.log(colors["Reset"]);
+      },
+      FgMagenta: (msg) => {
+        console.log(colors["FgMagenta"], msg);
+        console.log(colors["Reset"]);
+      },
+      FgCyan: (msg) => {
+        console.log(colors["FgCyan"], msg);
+        console.log(colors["Reset"]);
+      },
+      FgWhite: (msg) => {
+        console.log(colors["FgWhite"], msg);
+        console.log(colors["Reset"]);
+      },
+      BgBlack: (msg) => {
+        console.log(colors["BgBlack"], msg);
+        console.log(colors["Reset"]);
+      },
+      BgRed: (msg) => {
+        console.log(colors["BgRed"], msg);
+        console.log(colors["Reset"]);
+      },
+      BgGreen: (msg) => {
+        console.log(colors["BgGreen"], msg);
+        console.log(colors["Reset"]);
+      },
+      BgYellow: (msg) => {
+        console.log(colors["BgYellow"], msg);
+        console.log(colors["Reset"]);
+      },
+      BgBlue: (msg) => {
+        console.log(colors["BgBlue"], msg);
+        console.log(colors["Reset"]);
+      },
+      BgMagenta: (msg) => {
+        console.log(colors["BgMagenta"], msg);
+        console.log(colors["Reset"]);
+      },
+      BgCyan: (msg) => {
+        console.log(colors["BgCyan"], msg);
+        console.log(colors["Reset"]);
+      },
+      BgWhite: (msg) => {
+        console.log(colors["BgWhite"], msg);
+        console.log(colors["Reset"]);
+      },
+      stateSuccess: (msg) => {
+        console.log(colors["FgGreen"], "✔ Success:", msg);
+        console.log(colors["Reset"]);
+      },
+      stateFail: (msg) => {
+        console.log(colors["FgRed"], "💥 Fail:", msg);
+        console.log(colors["Reset"]);
+      },
+    },
+  };
+}
+
 function onSuccesCallWriteData(response, call) {
   const fsExtra = require("fs-extra");
   const stringedDataPrettified = JSON.stringify(
@@ -85,4 +219,5 @@ module.exports = {
   createNewConfig,
   createEndpointCallObj,
   createEndpointObj,
+  coLog,
 };
